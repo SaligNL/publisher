5f42d983d37cb01004e991520bdfb9dbc8c99dbd
Ludo Stellingwerff <ludo@almende.org>
2016-02-19 09:58:06 +0100

    Allow both array and object input

--- src/main/java/com/almende/salig/Client.java
+++ src/main/java/com/almende/salig/Client.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.logging.Logger;
 
 import org.joda.time.DateTime;
 
@@ -17,12 +18,15 @@ import com.almende.eve.protocol.jsonrpc.annotation.AccessType;
 import com.almende.eve.protocol.jsonrpc.formats.Params;
 import com.almende.util.jackson.JOM;
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 /**
  * The Class Client.
  */
 public class Client extends Agent {
+	private static final Logger	LOG	= Logger.getLogger(Client.class.getName());
 
 	protected void onReady() {
 		schedule("start", null, 1000);
@@ -38,20 +42,34 @@ public class Client extends Agent {
 					System.in));
 
 			try {
-				final ObjectNode input = (ObjectNode) JOM.getInstance()
-						.readTree(br);
+				final ArrayNode list = JOM.createArrayNode();
+				final JsonNode input = JOM.getInstance().readTree(br);
 				br.close();
-				
-				Params params = new Params();
-				//<type>/string
-				String licenseString = getConfig().get("licenseUrl").asText()+"/" + input.get("type").asText()
-						+ "/string";
-				params.add("message", createMessage(input,licenseString));
 
-				call(URI.create(getConfig().get("publisherUrl").asText()),
-						"sendMessage", params);
-				
-				schedule("shutdown",null,1000);
+				if (input.isObject()) {
+					list.add(input);
+				} else if (input.isArray()) {
+					list.addAll((ArrayNode) input);
+				} else {
+					LOG.warning("Incorrect input type:" + input);
+				}
+
+				for (JsonNode item : list) {
+					final ObjectNode node = (ObjectNode) item;
+					Params params = new Params();
+					// <type>/string
+					String licenseString = getConfig().get("licenseUrl")
+							.asText()
+							+ "/"
+							+ node.get("type").asText()
+							+ "/string";
+					params.add("message", createMessage(node, licenseString));
+
+					call(URI.create(getConfig().get("publisherUrl").asText()),
+							"sendMessage", params);
+				}
+
+				schedule("shutdown", null, 1000);
 			} catch (JsonParseException e) {
 				e.printStackTrace();
 			}
@@ -68,12 +86,12 @@ public class Client extends Agent {
 		message.setSerial(input.get("type").asText() + "Detector");
 		return message;
 	}
-	
+
 	/**
 	 * Shutdown.
 	 */
 	@Access(AccessType.PUBLIC)
-	public void shutdown(){
+	public void shutdown() {
 		this.destroy(false);
 		System.exit(0);
 	}
